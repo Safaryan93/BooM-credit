1d8206d449fde4f9083fb60ae789bca28015c4a7
Safaryan93 <armen.safs@gmail.com>
2023-06-06 04:27:31 -0700

    change mail

--- js/rang_script.js
+++ js/rang_script.js
@@ -19,4 +19,4 @@
 //   this.style.background = 'linear-gradient(to right, #f15c22 0%, #f15c22 ' + valueone + '%, #F2F9FF ' + valueone + '%, #F2F9FF 100%)'
 // };
 
-var rangeSlider=function(){var e=$(".range-slider"),n=$(".range-slider__range"),i=$(".range_slide_value");e.each(function(){n.on("input",function(){$(this).next(i).val(this.value),$(this).parents(".range-slider").children(".value_max_left").html(this.value+" դրամ․")})})};rangeSlider(),document.getElementById("myinput").oninput=function(){var e=(this.value-this.min)/(this.max-this.min)*100;this.style.background="linear-gradient(to right, #f15c22 0%, #f15c22 "+e+"%, #F2F9FF "+e+"%, #F2F9FF 100%)"};
+var rangeSlider=function(){var e=$(".range-slider"),n=$(".range-slider__range"),i=$(".range_slide_value");e.each(function(){n.on("input",function(){$(this).next(i).val(this.value),$(this).parents(".range-slider").children(".value_max_left").html(this.value+" դրամ․")})})};rangeSlider(),document.getElementById("myinput").oninput=function(){var e=(this.value-this.min)/(this.max-this.min)*100;this.style.background="linear-gradient(to right, #0c5202 0%, #0c5202 "+e+"%, #F2F9FF "+e+"%, #F2F9FF 100%)"};
--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 if(!isset($_POST['name']) || !isset($_POST['phone']) || !isset($_POST['email'])) {
-    header('https://boom-credit.am/fill_blanks_page.php');
+    header('https://boom-credit.com/fill_blanks_page.php');
 }else{
     $name = $_POST['name'];
     $phon = $_POST['phone'];
@@ -10,13 +10,13 @@ if(!isset($_POST['name']) || !isset($_POST['phone']) || !isset($_POST['email']))
     $gumar = $_POST['gumar'];
 
 
-    $to='top_credit2021@mail.ru';
+    $to='boomcredit2023@gmail.com';
     $subject='тема сообщений';
     $massege =" անուն: ".$name."\r\n հեռ: ".$phon."\r\n գրանցված աշխատող: ".$ashxatox."\r\n սև ցուցակ: ".$sev_cucak ."\r\n գումար: ".$gumar ." դր\r\n էլ․ հասցե: ".$email;
 
     mail ($to, $subject, $massege);
 
-    header('Location: https://boom-credit.am/fill_blanks_page.php');
+    header('Location: https://boom-credit.com/fill_blanks_page.php');
 }
 
 
